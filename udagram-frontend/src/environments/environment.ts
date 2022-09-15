3b98fc7e2122bd74605b39bb41b8f4ffbd415bea
Chukwuemelie Onuigbo <48865921+chuksonu700@users.noreply.github.com>
2022-09-15 09:47:23 +0100

    nn

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a4d2b265755b2461c8ecdcd2e94e2f90-950057430.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
